a7a57b347974973621de574bd5d1c5643654b9e7
Wintorez7 <liordx76@gmail.com>
2025-03-05 22:47:23 +0530

    creating new page

--- nextjs-project-5/src/app/user-management/page.js
+++ nextjs-project-5/src/app/user-management/page.js
@@ -1,3 +1,5 @@
+import AddNewUser from "@/components/add-new-user";
+
 
 
 function UserManagement(){
@@ -5,7 +7,9 @@ function UserManagement(){
         <div className="p-20 max-w-6xl">
             <div className="flex justify-between">
                 <h1>User Management</h1>
+                <AddNewUser/>
             </div>
+
         </div>
     )
 }
---
+++ nextjs-project-5/src/components/add-new-user/index.js
@@ -0,0 +1,24 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+
+function AddNewUser(){
+    return (
+        <div>
+            <Button>Add New User</Button>
+        </div>
+    )
+}
+
+export default AddNewUser
